13580e315f0c3898c83a5be531f1ef7acc5b5f58
monamohamed <mona.m.mohamed@gmail.com>
2020-12-08 21:03:06 -0500

    commit tree fe31527e3b19c24d5d18a44dfa3d3c3439fd6dee

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
 def unsafe?(speed)
-
+    if speed > 60
+        return true
+        
+    elsif
+        speed < 40
+        return true
+    else
+        return false    
+    end
 end
 
 
 
 def not_safe?(speed)
-	
+    speed < 40 || speed > 60 ? true : false
+
 end
-	
+	# age = 1
+#age < 2 ? "baby" : "not a baby" 
 
 
